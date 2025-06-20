96e5ed74132c4e4eb5d70ee9dc81a559d9db192e
harshithmateti <harshithmateti2005@gmail.com>
2025-06-20 22:54:53 +0530

    Remove node_modules folder

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,31 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const { readdirSync } = require('fs');
-const { server } = require('./environment.js'); // Import the backend URL
-
-const app = express();
-var userid = '4';
-const webhook = require('./webhook.js');
-
+const {readdirSync} = require('fs')
+const app = express()
+var userid='4';
+const webhook=require('./webhook.js');
+ 
 const { Webhook } = require('svix');
-require('dotenv').config();
-const PORT = process.env.PORT;
+require('dotenv').config()
+const PORT = process.env.PORT
+//middlewares
+app.use(express.json())
+app.use(cors())
+ 
 
-// Middleware setup
-app.use(express.json());
-app.use(cors());
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
-// Dynamically register routes
-readdirSync('./routes').map((route) => {
-  const routePath = `/api/v1/${route}`; 
- 
-  app.use(routePath, (req, res, next) => {
- 
-    const externalUrl = `${server}${routePath}`;
-    console.log('Forwarding request to:', externalUrl);
- 
-    next();
-  });
-
-  app.use(routePath, require('./routes/' + route));
-});
- 
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
 app.get('/test', (req, res) => {
   res.json({ message: 'Backend is working!' });
 });
- 
-const startServer = () => {
-  db();  
-  app.listen(PORT, () => {
-    console.log('Server listening on port:', PORT);
-  });
-};
 
- 
-startServer();
+
+server()
+
