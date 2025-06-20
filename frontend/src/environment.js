1c01be4fbdeec7e700035a35d91c9a6d23cdca68
harshithmateti <harshithmateti2005@gmail.com>
2025-06-20 22:47:40 +0530

    corrected url errors

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
-let IS_PROD = true;
+// ./environment.js
+let IS_PROD = true; // Switch this for prod vs dev
 const server = IS_PROD ?
     "https://smartspender-4900.onrender.com" :
+    "http://localhost:8000";
 
-    "http://localhost:8000"
-
- 
+module.exports = { server };  
