d8ff10c7ab9e6e228a9044d5f1ad7c569bb98dd2
harshithmateti <harshithmateti2005@gmail.com>
2025-06-20 22:22:08 +0530

    added render url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,8 +1,9 @@
 import { useUser } from "@clerk/clerk-react";
 import React, { useContext, useState } from "react";
 import axios from "axios";
+import { server } from '../environment.js';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1/";
+const BASE_URL = `${server}/api/v1/`;
 
 const GlobalContext = React.createContext();
 
---
+++ frontend/src/environment.js
@@ -0,0 +1,7 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://smartspender-4900.onrender.com" :
+
+    "http://localhost:8000"
+
+ 
